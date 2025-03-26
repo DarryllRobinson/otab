670c5f6f35b691b058837790db9f0193809b3c22
DarryllRobinson <darryllrobinson@icloud.com>
2025-03-26 13:00:36 +1000

    Enhance Competition components with improved styling and layout; update CompetitionList to manage selected competition state.

--- src/features/Competitions/CompetitionDetails.js
+++ src/features/Competitions/CompetitionDetails.js
@@ -1,19 +1,38 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 export default function CompetitionDetails(props) {
   const { id, name, numTiles } = props;
 
   return (
-    <Box>
-      <Box>CompetitionDetails name: {name}</Box>
+    <Box
+      sx={{
+        padding: 4,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[700],
+        borderRadius: 2,
+        boxShadow: 3,
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        Competition: {name}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Number of Tiles: {numTiles}
+      </Typography>
       <Button
         key={id}
         component={RouterLink}
         to="/play"
         state={{ compId: id, create: true, numTiles }}
+        variant="contained"
+        color="primary"
+        sx={{ marginTop: 2 }}
       >
-        Enter the competition!
+        Enter the Competition!
       </Button>
     </Box>
   );
